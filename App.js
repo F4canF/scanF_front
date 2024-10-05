230b4424e3615f3a00fa37a9a334c188f1bc06b8
soeun823 <bsoeun866@gmail.com>
2024-10-05 14:55:48 +0900

    docs)컴포언트 추가

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
 // App.js
 import React, { useState, useEffect } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
-import LoadingScreen from './component/Loading'; // 별도 로딩 컴포넌트 가져오기
 import Loading from './component/Loading';
 
 const App = () => {
@@ -9,7 +8,7 @@ const App = () => {
 
   // 2초 동안 강제로 로딩 화면을 표시한 후 로딩 종료
   useEffect(() => {
-    const timer = setTimeout(() => setLoading(false), 2000); // 2초 후 로딩 상태 해제
+    const timer = setTimeout(() => setLoading(false), 3000); // 3초 후 로딩 상태 해제
     return () => clearTimeout(timer); // 컴포넌트 언마운트 시 타이머 정리
   }, []);
 
--- component/Loading.js
+++ component/Loading.js
@@ -20,22 +20,20 @@ const styles = StyleSheet.create({
         backgroundColor: '#fff', 
     },
     titleText: {
-        color: '#F8F152', // 문자열로 수정
-        textStrokeWidth: 4, // -webkit-text-stroke-width를 textStrokeWidth로 변경
-        textStrokeColor: '#000', // -webkit-text-stroke-color를 textStrokeColor로 변경
-        fontFamily: 'Agbalumo', // font-family를 fontFamily로 변경
-        fontSize: '7rem', // px 제거 및 값 조정
-        fontStyle: 'normal', // 정상
+        color: '#F8F152', 
+        WebkitTextStrokeWidth: '2px', // 사용하려면 Webkit 접두사를 추가
+        WebkitTextStrokeColor: '#000000',
+        fontFamily: 'Agbalumo', 
+        fontSize: '7rem', 
         fontWeight: '400', 
         lineHeight: 'normal', 
     },
     text: {
-        color: 'rgba(85, 85, 85, 0.87)', // RGBA 문자열로 지정
-        fontFamily: 'Jua', // font-family를 fontFamily로 변경
-        fontSize: '2.4rem', // font-size를 fontSize로 변경
-        fontStyle: 'normal', // font-style을 fontStyle로 변경
-        fontWeight: '200', // font-weight를 fontWeight로 변경
-        lineHeight: 100, // line-height는 숫자로 지정, 필요에 따라 조정 가능
+        color: 'rgba(85, 85, 85, 0.87)', 
+        fontFamily: 'Jua', 
+        fontSize: '2.4rem', 
+        fontStyle: 'normal', 
+        fontWeight: '200', 
     },
 });
 
